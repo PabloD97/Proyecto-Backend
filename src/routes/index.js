b30367800834515309be54f8529789cf901c0a6e
PabloD97 <pablo97758@gmail.com>
2021-09-04 17:24:03 -0300

    chore: Endpoints
    - Se agregaron los endpoints para las rutas de idea, user y comment

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,9 +6,14 @@ const helmet = require("helmet"); // leer la documentacion de helmet.js
 const compression = require("compression"); // ayuda a comprimir las peticiones HTTP para que sea mas rapido
 require("express-async-errors"); // ayuda a capturar en un middleware las excepciones asincronas que producen las promesas normalmente.
 
-const { NotFoundMiddleware, ErrorMiddleware } = require('../middlewares');
+const { NotFoundMiddleware, ErrorMiddleware } = require("../middlewares");
 
-module.exports = function ({ HomeRoutes }) {
+module.exports = function ({
+  HomeRoutes,
+  UserRoutes,
+  IdeaRoutes,
+  CommentRoutes,
+}) {
   const router = express.Router();
   const apiRoutes = express.Router();
 
@@ -20,16 +25,20 @@ module.exports = function ({ HomeRoutes }) {
   apiRoutes.use(express.json()).use(cors()).use(helmet()).use(compression());
   // esto son los default middleware.
 
-  apiRoutes.use('/home', HomeRoutes);
+  apiRoutes.use("/home", HomeRoutes);
+  apiRoutes.use("/user", UserRoutes);
+  apiRoutes.use("/idea", IdeaRoutes);
+  apiRoutes.use("/comment", CommentRoutes);
+
 
   apiRoutes.use(NotFoundMiddleware);
   apiRoutes.use(ErrorMiddleware);
 
-  router.use('/v1/api', apiRoutes);
+  router.use("/v1/api", apiRoutes);
   // ESto lo tenemos que configurar una sola vez para cada una de las rutas
   // luego ya con la practica se vuelve mas facil.
 
   // Gracias a tener todo ordenador, va ser mas facil escalar.
 
-  return router
+  return router;
 };
