95cc762dcd0b2518fb2bc5bb3bd47dbc3a5a892b
PabloD97 <pablo97758@gmail.com>
2021-09-06 19:52:08 -0300

    chore: Auth Container
    - Se registro AuthService en container para luego poder ser inyectado con awilix
    - Se registro AuthController en container para luego poder ser inyectado con awilix
    - Se registro AuthRoutes en container para luego poder ser inyectado con awilix

--- src/startup/container.js
+++ src/startup/container.js
@@ -5,62 +5,86 @@ const { createContainer, asClass, asValue, asFunction } = require("awilix");
 
 // config
 const config = require("../config");
-const app = require('.')
+const app = require(".");
 
 // services
-const { HomeService, UserService, IdeaService, CommentService } = require("../services");
-
+const {
+  HomeService,
+  UserService,
+  IdeaService,
+  CommentService,
+  AuthService,
+} = require("../services");
 
 // controllers
-const { HomeController, UserController, IdeaController, CommentController } = require("../controllers");
+const {
+  HomeController,
+  UserController,
+  IdeaController,
+  CommentController,
+  AuthController,
+} = require("../controllers");
 
 // routes
-const { HomeRoutes, UserRoutes, IdeaRoutes, CommentRoutes } = require("../routes/index.routes");
+const {
+  HomeRoutes,
+  UserRoutes,
+  IdeaRoutes,
+  CommentRoutes,
+  AuthRoutes,
+} = require("../routes/index.routes");
 const Routes = require("../routes");
 
 // models
-const { User, Idea, Comment } = require('../models')
+const { User, Idea, Comment } = require("../models");
 const container = createContainer();
 
 // repositories
-const { UserRepository, IdeaRepository, CommentRepository } = require('../repositories');
+const {
+  UserRepository,
+  IdeaRepository,
+  CommentRepository,
+} = require("../repositories");
 
 container
   .register({
     // configuracion principal de la aplicacion
     app: asClass(app).singleton(),
     router: asFunction(Routes).singleton(),
-    config: asValue(config)
+    config: asValue(config),
   })
   .register({
     HomeService: asClass(HomeService).singleton(),
     UserService: asClass(UserService).singleton(),
     IdeaService: asClass(IdeaService).singleton(),
     CommentService: asClass(CommentService).singleton(),
-
+    AuthService: asClass(AuthService).singleton(),
   })
   .register({
     HomeController: asClass(HomeController.bind(HomeController)).singleton(),
     UserController: asClass(UserController.bind(UserController)).singleton(),
     IdeaController: asClass(IdeaController.bind(IdeaController)).singleton(),
-    CommentController: asClass(CommentController.bind(CommentController)).singleton(),
+    CommentController: asClass(
+      CommentController.bind(CommentController)
+    ).singleton(),
+    AuthController: asClass(AuthController.bind(AuthController)).singleton(),
   })
   .register({
     HomeRoutes: asFunction(HomeRoutes).singleton(), //-> lo pasamos como funcion, xque fue una funcion lo que exportamos de home.routes.js
-    UserRoutes: asFunction(UserRoutes).singleton(), 
-    IdeaRoutes: asFunction(IdeaRoutes).singleton(), 
-    CommentRoutes: asFunction(CommentRoutes).singleton(), 
-
+    UserRoutes: asFunction(UserRoutes).singleton(),
+    IdeaRoutes: asFunction(IdeaRoutes).singleton(),
+    CommentRoutes: asFunction(CommentRoutes).singleton(),
+    AuthRoutes: asFunction(AuthRoutes).singleton(),
   })
   .register({
     User: asValue(User),
     Idea: asValue(Idea),
-    Comment: asValue(Comment)
+    Comment: asValue(Comment),
   })
   .register({
     UserRepository: asClass(UserRepository).singleton(),
     IdeaRepository: asClass(IdeaRepository).singleton(),
-    CommentRepository: asClass(CommentRepository).singleton() 
+    CommentRepository: asClass(CommentRepository).singleton(),
   });
 
 module.exports = container;
