1d82f27901d1c0b4ce260ed828182df11a98a430
PabloD97 <pablo97758@gmail.com>
2021-09-07 16:35:42 -0300

    fix: Endpoints
    - Se arreglo el endpoint de las rutas de idea.route y user.route

--- src/routes/idea.routes.js
+++ src/routes/idea.routes.js
@@ -6,7 +6,7 @@ module.exports = function ({ IdeaController }) {
   router.get("", IdeaController.getAll);
   router.get("/:ideaId", IdeaController.get);
   router.get("/:userId/all", IdeaController.getUserIdea);
-  router.post("", IdeaController.create);
+  router.post("/", IdeaController.create);
   router.patch("/:ideaId", IdeaController.update);
   router.delete("/:ideaId", IdeaController.delete);
   router.post('/:ideaId/upvote', IdeaController.upvoteIdea);
