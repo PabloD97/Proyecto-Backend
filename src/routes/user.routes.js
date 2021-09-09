1295abdf656264ac55f5b915601a2669b03084cd
PabloD97 <pablo97758@gmail.com>
2021-09-09 00:13:10 -0300

    style: UserRoutes
    - Se agregaron espacios, punto y coma, etc.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,23 @@
 const { Router } = require("express");
-const { AuthMiddleware, ParseIntMiddleware, CacheMiddleware } = require('../middlewares');
+const {
+  AuthMiddleware,
+  ParseIntMiddleware,
+  CacheMiddleware,
+} = require("../middlewares");
 
-const { CACHE_TIME } = require('../helpers');
+const { CACHE_TIME } = require("../helpers");
 
 module.exports = function ({ UserController }) {
   const router = Router();
 
   router.get("/:userId", UserController.get);
-  router.get("/", [ ParseIntMiddleware, CacheMiddleware(CACHE_TIME.ONE_HOUR)] ,UserController.getAll);
-  router.patch("/:userId",AuthMiddleware ,UserController.update);
-  router.delete("/:userId", AuthMiddleware,UserController.delete);
-
+  router.get(
+    "/",
+    [ParseIntMiddleware],
+    UserController.getAll
+  );
+  router.patch("/:userId", AuthMiddleware, UserController.update);
+  router.delete("/:userId", AuthMiddleware, UserController.delete);
 
   return router;
 };
